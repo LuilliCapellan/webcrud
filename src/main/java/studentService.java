a3136483657e79656c6f3638b209068004726aed
Luis Capellan <luillicapellan29@gmail.com>
2019-02-11 22:34:19 -0500

    algunos fallos y cosas vacias

--- src/main/java/studentService.java
+++ src/main/java/studentService.java
@@ -1,28 +1,34 @@
 import java.util.List;
 
 public class studentService implements studentDAO {
+    private RunStudentDAO runStudentDAO;
+
+    public studentService() {
+        runStudentDAO = new RunStudentDAO();
+    }
+
     @Override
     public void insert(student e) {
-
+        runStudentDAO.insert(e);
     }
 
     @Override
     public void update(student e) {
-
+        runStudentDAO.update(e);
     }
 
     @Override
     public void delete(student e) {
-
+        runStudentDAO.delete(e);
     }
 
     @Override
     public List<student> getAll() {
-        return null;
+        return  runStudentDAO.getAll();
     }
 
     @Override
     public student getByMatricula(int matricula) {
-        return null;
+        return runStudentDAO.getByMatricula(matricula);
     }
 }
